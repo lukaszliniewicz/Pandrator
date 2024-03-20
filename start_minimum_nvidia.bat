728ede941ee4e4d5a2fc810f08a3398a66791dfc
lukaszliniewicz <75737665+lukaszliniewicz@users.noreply.github.com>
2024-03-20 20:00:35 +0200

    Update start_minimum_nvidia.bat

--- start_minimum_nvidia.bat
+++ start_minimum_nvidia.bat
@@ -10,25 +10,19 @@ if %errorlevel% neq 0 (
     REM Download Miniconda installer
     curl -O https://repo.anaconda.com/miniconda/Miniconda-latest-Windows-x86_64.exe
     REM Install Miniconda silently in a subfolder
-    start /wait "" Miniconda-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%BASE_DIR%\Audiobook-Generator\miniconda
+    start /wait "" Miniconda-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%BASE_DIR%\miniconda
     REM Add Miniconda to PATH
-    set PATH=%BASE_DIR%\Audiobook-Generator\miniconda;%BASE_DIR%\Audiobook-Generator\miniconda\Scripts;%PATH%
+    set PATH=%BASE_DIR%\miniconda;%BASE_DIR%\miniconda\Scripts;%PATH%
 )
-
-REM Clone the Audiobook-Generator repository
-if not exist Audiobook-Generator (
-    git clone https://github.com/lukaszliniewicz/Audiobook-Generator.git
-)
-cd Audiobook-Generator
 
 REM Create and activate audiobook_generator conda environment
-if not exist audiobook_generator (
+if not exist audiobook_generator\audiobook_generator (
     conda create -y -n audiobook_generator python=3.10
 )
 call conda activate audiobook_generator
 
 REM Install requirements for audiobook_generator
-pip install -r requirements.txt
+pip install -r audiobook_generator\requirements.txt
 
 REM Clone the xtts-api-server repository
 if not exist Tools\xtts-api-server (
@@ -49,8 +43,9 @@ pip install xtts-api-server
 pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 REM Start the xtts server
-start "XTTS Server" cmd /c "call conda activate xtts_api_server & python -m xtts_api_server --deepspeed"
+start "XTTS Server" cmd /c "call conda activate xtts_api_server & cd Tools\xtts-api-server & python -m xtts_api_server --deepspeed"
 
 REM Start the audiobook_generator
 call conda activate audiobook_generator
+cd audiobook_generator
 python audiobook_generator.py
