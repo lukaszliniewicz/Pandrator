9ca1ccdb1f3dd868a554d292e2781400d482fa8e
Lukasz Liniewicz <l.liniewicz@gmail.com>
2026-08-23 01:25:23 +0200

    Release 0.8.13 with reviewed pronunciations

--- web/src/lib/api.generated.ts
+++ web/src/lib/api.generated.ts
@@ -2206,7 +2206,7 @@ export interface components {
             api_version?: string;
             /**
              * Application Version
-             * @default 0.8.12
+             * @default 0.8.13
              */
             application_version?: string;
             /** Canonical Origin */
