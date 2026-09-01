623475acfc40484feefd0e53bdbab692ede2fda1
Lukasz Liniewicz <l.liniewicz@gmail.com>
2026-09-01 02:49:17 +0200

    Refresh 0.8.17 web bundle

---
+++ pandrator/web/static/_app/immutable/entry/start.ClFegfKG.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/ULGe3XOc.js";export{o as load_css,r as start};
--- pandrator/web/static/_app/immutable/entry/start.Ds0cPUhh.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DEEvq0xr.js";export{o as load_css,r as start};
